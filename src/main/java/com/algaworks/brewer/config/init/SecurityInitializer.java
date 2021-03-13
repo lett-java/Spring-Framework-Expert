8165ea10b81c19e4ab55f2643555f321f2228859
Fabio Lettieri <fabiolettieri09@gmail.com>
2021-03-13 11:26:04 -0300

    Adicionada web.xml com session-timeout, que faz a sessão expirar após um determinado tempo, redirecionamento para a tela de login

--- src/main/java/com/algaworks/brewer/config/init/SecurityInitializer.java
+++ src/main/java/com/algaworks/brewer/config/init/SecurityInitializer.java
@@ -1,7 +1,10 @@
 package com.algaworks.brewer.config.init;
 
+import java.util.EnumSet;
+
 import javax.servlet.FilterRegistration;
 import javax.servlet.ServletContext;
+import javax.servlet.SessionTrackingMode;
 
 import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
 import org.springframework.web.filter.CharacterEncodingFilter;
@@ -10,6 +13,9 @@ public class SecurityInitializer extends AbstractSecurityWebApplicationInitializ
 	
 	@Override
 	protected void beforeSpringSecurityFilterChain(ServletContext servletContext) {
+//		servletContext.getSessionCookieConfig().setMaxAge(20);
+		servletContext.setSessionTrackingModes(EnumSet.of(SessionTrackingMode.COOKIE));
+
 		FilterRegistration.Dynamic caracterEncodingFilter = servletContext.addFilter("encodingFilter", 
 				new CharacterEncodingFilter());
 		caracterEncodingFilter.setInitParameter("encoding", "UTF-8");
