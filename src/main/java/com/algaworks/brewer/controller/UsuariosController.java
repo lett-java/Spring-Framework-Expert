c69ec635e4f607a5e11b70441b00b74db0ace548
Fabio Lettieri <fabiolettieri09@gmail.com>
2021-03-15 07:48:39 -0300

    Protótipo da pesquisa de Usuarios

--- src/main/java/com/algaworks/brewer/controller/UsuariosController.java
+++ src/main/java/com/algaworks/brewer/controller/UsuariosController.java
@@ -5,6 +5,7 @@ import javax.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
@@ -12,6 +13,8 @@ import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.algaworks.brewer.model.Usuario;
 import com.algaworks.brewer.repository.Grupos;
+import com.algaworks.brewer.repository.Usuarios;
+import com.algaworks.brewer.repository.filter.UsuarioFilter;
 import com.algaworks.brewer.service.CadastroUsuarioService;
 import com.algaworks.brewer.service.exception.EmailUsuarioJaCadastradoException;
 import com.algaworks.brewer.service.exception.SenhaObrigatoriaUsuarioException;
@@ -19,26 +22,29 @@ import com.algaworks.brewer.service.exception.SenhaObrigatoriaUsuarioException;
 @Controller
 @RequestMapping("/usuarios")
 public class UsuariosController {
-	
+
 	@Autowired
 	private CadastroUsuarioService cadastroUsuarioService;
 
 	@Autowired
 	private Grupos grupos;
 	
+	@Autowired
+	private Usuarios usuarios;
+
 	@RequestMapping("/novo")
 	public ModelAndView novo(Usuario usuario) {
 		ModelAndView mv = new ModelAndView("usuario/CadastroUsuario");
 		mv.addObject("grupos", grupos.findAll());
 		return mv;
 	}
-	
+
 	@PostMapping("/novo")
 	public ModelAndView salvar(@Valid Usuario usuario, BindingResult result, RedirectAttributes attributes) {
 		if (result.hasErrors()) {
 			return novo(usuario);
 		}
-		
+
 		try {
 			cadastroUsuarioService.salvar(usuario);
 		} catch (EmailUsuarioJaCadastradoException e) {
@@ -48,9 +54,16 @@ public class UsuariosController {
 			result.rejectValue("senha", e.getMessage(), e.getMessage());
 			return novo(usuario);
 		}
-		
+
 		attributes.addFlashAttribute("mensagem", "Usuário salvo com sucesso");
 		return new ModelAndView("redirect:/usuarios/novo");
 	}
-	
+
+	@GetMapping
+	public ModelAndView pesquisar(UsuarioFilter usuarioFilter) {
+		ModelAndView mv = new ModelAndView("/usuario/PesquisaUsuarios");
+		mv.addObject("usuarios", usuarios.filtrar(usuarioFilter));
+		mv.addObject("grupos", grupos.findAll());
+		return mv;
+	}
 }
