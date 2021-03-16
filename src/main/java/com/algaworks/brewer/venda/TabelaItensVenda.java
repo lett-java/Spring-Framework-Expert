dde19ab572a7f5fbcc2276bdfa8fe06309b91fc2
Fabio Lettieri <fabiolettieri09@gmail.com>
2021-03-16 14:16:59 -0300

    Criação dos das Classes ItemVenda e TabelaItensVenda, com testes TabelaItensVendaTest

---
+++ src/main/java/com/algaworks/brewer/venda/TabelaItensVenda.java
@@ -0,0 +1,29 @@
+package com.algaworks.brewer.venda;
+
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.List;
+
+import com.algaworks.brewer.model.Cerveja;
+import com.algaworks.brewer.model.ItemVenda;
+
+public class TabelaItensVenda {
+	
+	List<ItemVenda> itens = new ArrayList<ItemVenda>();
+	
+	public BigDecimal getValorTotal() {
+		return itens.stream()
+				.map(ItemVenda::getValorTotal)
+				.reduce(BigDecimal::add)
+				.orElse(BigDecimal.ZERO);
+	}
+	
+	public void adicionarItem(Cerveja cerveja, Integer quantidade) {
+		ItemVenda itemVenda = new ItemVenda();
+		itemVenda.setCerveja(cerveja);
+		itemVenda.setQuantidade(quantidade);
+		itemVenda.setValorUnitario(cerveja.getValor());
+		
+		itens.add(itemVenda);
+	}
+}
